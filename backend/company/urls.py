f26529a1ab5c360a82ed1a37b8f07141d75db908
Mahesh-aadhitya <maheshaadhitya2112@gmail.com>
2024-10-15 13:11:11 +0530

    Database Config and Post API code setup

--- backend/company/urls.py
+++ backend/company/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CompanyCreateView
+from .views import CompanyCreateView, CompanyListView
 
 urlpatterns = [
+    path('company/', CompanyListView.as_view(), name='company-list'),
     path('company/', CompanyCreateView.as_view(), name='company-create'),
 ]
--- backend/company/views.py
+++ backend/company/views.py
@@ -1,8 +1,11 @@
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import CompanySerializer
+from .models import Company
 
+
+#POST API 
 class CompanyCreateView(APIView):
     def post(self, request):
         serializer = CompanySerializer(data=request.data)
@@ -10,3 +13,8 @@ class CompanyCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+#GET API
+class CompanyListView(generics.ListAPIView):
+    queryset = Company.objects.all()  # Retrieve all Company records
+    serializer_class = CompanySerializer
