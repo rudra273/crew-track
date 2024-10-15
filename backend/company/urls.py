6c4da9cb64edeef8bc843ec7afc44fe2026a5c33
Mahesh-aadhitya <maheshaadhitya2112@gmail.com>
2024-10-15 14:57:47 +0530

    GET API Update

--- backend/company/urls.py
+++ backend/company/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CompanyCreateView, CompanyListView, CompanyDetailView
 
 urlpatterns = [
-    path('company/', CompanyListView.as_view(), name='company-list'),
+    path('companies/', CompanyListView.as_view(), name='company-list'),
     path('company/', CompanyCreateView.as_view(), name='company-create'),
     path('companies/<int:pk>/', CompanyDetailView.as_view(), name='company-detail'),
 ]
