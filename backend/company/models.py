a3d9a8a682a22466efd1a0e6eefdc38d822f142e
rudra273 <rudramohanty45@gmail.com>
2024-10-15 18:14:55 +0530

    Create auth

--- backend/company/models.py
+++ backend/company/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Company(models.Model):
     id = models.CharField(max_length=10, primary_key=True, unique=True, editable=False)  # Format: [131124] dd-mm-yy-uuidchars
@@ -8,6 +9,8 @@ class Company(models.Model):
     description = models.TextField(null=True, blank=True, max_length=255)  # Accepts null and can be left blank
     since = models.DateField(null=True, blank=True)  # Accepts null and can be left blank
     employee_range = models.CharField(max_length=50, null=True, blank=True)   # Example: "10-50", "50-100", etc.
+
+    # user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
