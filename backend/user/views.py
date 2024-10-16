2566a3e2ce2e8dde908eb761c5807cf67caca0b4
Mahesh-aadhitya <maheshaadhitya2112@gmail.com>
2024-10-16 20:37:17 +0530

    API Endpoints update

--- backend/user/views.py
+++ backend/user/views.py
@@ -2,8 +2,6 @@ from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserRegisterSerializer
-from django.contrib.auth.models import User
-from rest_framework_simplejwt.tokens import RefreshToken
 import logging
 # Create your views here.
 logger = logging.getLogger(__name__)
@@ -14,18 +12,17 @@ class UserRegisterView(APIView):
         serializer = UserRegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            refresh = RefreshToken.for_user(user)
             user_data = {
                 'user_id': user.id,
-                'username': user.username
+                'username': user.username,
+                'email': user.email
             }
-            # Return response with tokens
             logger.info("User Registered Successfully")
             return Response({
-                'user_data': user_data ,
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
+                'message': "User Registered successfully",
+                'user_data': user_data
             }, status=status.HTTP_201_CREATED)
+         
         logger.error("User Registration Failed")
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
